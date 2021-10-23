baa9a360a58d08e27aa5b804b6c03e870290d5ba
Kimberley Salter <ksalter@Kimberleys-MacBook-Pro.local>
2021-10-23 16:43:20 +0100

    added & migrated models

--- barbiemovies/models.py
+++ barbiemovies/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class BarbieMovies(models.Model):
+    title = models.CharField(max_length=70)
+    year = models.CharField(max_length=10)
+    barbies_char = models.CharField(max_length=70)
+    animal_friend = models.CharField(max_length=70)
