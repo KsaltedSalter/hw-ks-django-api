5f79c1c877e210a7cc98222871523f44bb1c6c7a
Kimberley Salter <ksalter@Kimberleys-MacBook-Pro.local>
2021-10-23 17:05:10 +0100

    added views/urls & index.html checked browser

--- barbiemovies/views.py
+++ barbiemovies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from barbiemovies.models import BarbieMovies
 
 # Create your views here.
+def index(request):
+    list = BarbieMovies.objects.all()
+    context = {'babrbiemovies': list}
+    return render(request, 'barbiemovies/index.html', context)
